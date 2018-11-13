111d500acd66f249315766ce9d7e82000c2b36d8
Rafreaky Weyland <darklighterk25@gmail.com>
2018-11-12 19:58:26 -0600

    Navbar ya funciona con los cambios a la API

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   imports: [
     CommonModule,
     MatProgressBarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ],
   exports: [
     MatProgressBarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ],
   declarations: []
 })
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { faMoneyBill, faShoppingCart, faStore, faSignOutAlt, faUserAlt, faUtensils } from '@fortawesome/free-solid-svg-icons';
 import { AccountsService } from '../../../services/accounts/accounts.service';
+import { MatSnackBar } from '@angular/material';
+import { MatSnackBarHorizontalPosition } from "@angular/material/snack-bar";
+import { MatSnackBarVerticalPosition } from "@angular/material/snack-bar";
 import { Order } from '../../../interfaces/order';
 import { OrdersService } from '../../../services/orders/orders.service';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-navbar',
@@ -19,6 +21,10 @@ export class NavbarComponent implements OnInit {
   user_alt = faUserAlt;
   utensils = faUtensils;
 
+  // Posición de las alertas
+  horizontalPosition: MatSnackBarHorizontalPosition = 'right';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
   cart: Order;
   isEmployee: Boolean = false;
   isLogged: Boolean = false;
@@ -26,34 +32,51 @@ export class NavbarComponent implements OnInit {
   userID: number;
 
   constructor( private _accountsService: AccountsService,
-               private _ordersService: OrdersService ) {
+               private _ordersService: OrdersService,
+               public snackBar: MatSnackBar ) {
   }
 
   ngOnInit() {
     this._ordersService.getCart().subscribe( ( cart: Order ) => {
       this.cart = cart;
     });
-    this._accountsService.logged().subscribe( ( response ) => {
-      this.isLogged = response['logged_in'] === 'true';
-      this.isEmployee = response['employee'] === 'true';
-      this.userID = response['id_user'];
-      this.loading = false;
+    this._accountsService.logged().subscribe(
+      response => {
+        this.isLogged = response['logged_in'] === 'true';
+        this.isEmployee = response['employee'] === 'true';
+        this.userID = response['id_user'];
+        this.loading = false;
+      }
+    );
+  }
+
+  alert( message: string ) {
+    this.snackBar.open(message, null, {
+      duration: 2000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
     });
   }
 
   login( email, password ) {
     this.loading = true;
     this._accountsService.login( { email: email, password: password } )
-      .subscribe( ( response ) => {
-        this.isLogged = response['logged_in'] === 'true';
-        this.isEmployee = response['employee'] === 'true';
-        this.userID = response['id_user'];
-        this.loading = false;
-      });
+      .subscribe(
+        response => {
+          this.isLogged = response['logged_in'] === 'true';
+          this.isEmployee = response['employee'] === 'true';
+          this.userID = response['id_user'];
+          this.loading = false;
+        },
+        error => {
+          this.alert( '¡Usuario inválido!' );
+          this.loading = false;
+        }
+      );
   }
 
   logout() {
-    this._accountsService.logout().subscribe( ( response ) => {
+    this._accountsService.logout().subscribe( response => {
       this.isLogged = response['logged_in'] === 'true';
       this.isEmployee = response['employee'] === 'true';
       this.userID = response['id_user'];
